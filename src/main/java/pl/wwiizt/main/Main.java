0e1099b75cc4fd97faab9665531f68af75c09e76
Adam Mróz <sadam36@gmail.com>
2013-04-06 17:55:43 +0200

    parsing command line using apache commons cli

--- src/main/java/pl/wwiizt/main/Main.java
+++ src/main/java/pl/wwiizt/main/Main.java
@@ -3,7 +3,12 @@ package pl.wwiizt.main;
 import java.io.File;
 import java.util.List;
 
-import org.apache.log4j.Logger;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,32 +17,75 @@ import pl.wwiizt.search.service.SearchEngineService;
 
 public class Main {
 
-	private static final Logger logger = Logger.getLogger(Main.class);
 	private static ApplicationContext appContext;
+
+	private final static String SEARCH = "search";
+	private final static String INDEX = "index";
+	private final static String CONVERT = "convert";
+	private final static String HELP = "help";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		appContext = new ClassPathXmlApplicationContext(
-				new String[] { "applicationContext.xml" });
-		if (args != null && args.length > 1 && "convert".equals(args[0])) {
+		Options options = getOptions();
+		CommandLine cmd = parseCommandLine(options, args);
+
+		if (args.length <= 1 || cmd.hasOption(HELP)) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("wwiizt", options);
+			System.exit(1);
+		}
+
+		appContext = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
+
+		if (cmd.hasOption(CONVERT)) {
 			CclService service = appContext.getBean(CclService.class);
-			service.convertFilesToPlainText(new File(args[1]));
+			service.convertFilesToPlainText(new File(cmd.getOptionValue(CONVERT)));
 		}
-		
-		if (args != null && args.length > 1 && "index".equals(args[0])) {
+
+		if (cmd.hasOption(INDEX)) {
 			SearchEngineService service = appContext.getBean(SearchEngineService.class);
-			service.index(new File(args[1]));
+			service.index(new File(cmd.getOptionValue(INDEX)));
 		}
-		
-		if (args != null && args.length > 1 && "search".equals(args[0])) {
+
+		if (cmd.hasOption(SEARCH)) {
 			SearchEngineService service = appContext.getBean(SearchEngineService.class);
-			List<String> hits = service.search(args[1]);
-			for (String hit : hits) {
-				System.out.println(hit);
+			List<String> hits = service.search(cmd.getOptionValue(SEARCH));
+
+			System.out.println("\n=========================== Search results: ");
+			
+			if (hits.isEmpty())
+				System.out.println("No results found");
+			else {
+				for (String hit : hits)
+					System.out.println(hit);
 			}
 		}
+	}
+
+	private static Options getOptions() {
+		Options options = new Options();
+
+		options.addOption(SEARCH, "s", true, "search query");
+		options.addOption(INDEX, "i", true, "index path");
+		options.addOption(CONVERT, "i", true, "convert path");
+		options.addOption(HELP, "h", false, "help");
+
+		return options;
+	}
+
+	private static CommandLine parseCommandLine(Options options, String[] args) {
+		CommandLineParser cmdParser = new PosixParser();
+		CommandLine result = null;
+		try {
+			result = cmdParser.parse(options, args);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		return result;
 	}
 
 }
