094e5dde96e5d52a9dd825d46d9bb785dcc711a6
Adam Mróz <sadam36@gmail.com>
2013-04-08 23:16:18 +0200

    Elastic search 0.9, script for create indexes, small fix

---
+++ createIndexes.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Na podstawie:
+#http://www.elasticsearch.org/guide/reference/index-modules/similarity/
+#http://www.elasticsearch.org/guide/reference/api/admin-indices-create-index/
+#http://www.elasticsearch.org/guide/reference/mapping/core-types/
+#http://www.elasticsearch.org/guide/reference/java-api/index_/
+
+
+#DEFAULT
+curl -XPUT 'http://localhost:9200/wiki/' -d '';
+
+#BM25
+curl -XPUT 'http://localhost:9200/wikiib/' -d '{                           
+    "settings" : {
+        "index" : {
+            "similarity" : "BM25"
+        }
+    }
+}';
+
+#DRF - divergence from randomness
+ curl -XPUT 'http://localhost:9200/wikiib/' -d '{                           
+    "settings" : {
+        "index" : {
+            "similarity" : "DRF"
+        }
+    }
+}';
+
+
+#Information Based Model
+curl -XPUT 'http://localhost:9200/wikiib/' -d '{                           
+    "settings" : {
+        "index" : {
+            "similarity" : "IB"
+        }
+    }
+}';
--- src/main/java/pl/wwiizt/search/service/SearchEngineService.java
+++ src/main/java/pl/wwiizt/search/service/SearchEngineService.java
@@ -72,6 +72,7 @@ public class SearchEngineService {
 			File[] files = dir.listFiles(new XmlFileFilter());
 			if (files != null) {
 				for(File file : files) {
+					System.out.println(file);
 					ChunkList cl = cclService.loadFile(file);
 					
 					if (cl != null) {
@@ -99,6 +100,7 @@ public class SearchEngineService {
 //				.should(QueryBuilders.queryString(query).field(FIELD_BASE_PLAIN_TEXT).boost(BOOST_FIELD_BASE_PLAIN_TEXT))
 				.should(QueryBuilders.queryString(query).field(FIELD_PLAIN_TEXT).boost(BOOST_FIELD_PLAIN_TEXT));
 				
+//		client.admin().cluster().prepareHealth().setWaitForGreenStatus().execute().actionGet(); 
 		
 		SearchResponse response = client.prepareSearch(indexName)
 		        .setTypes(TYPE_NAME)
