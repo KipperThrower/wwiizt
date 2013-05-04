6986ec2c6b97371dcae7cc930fc833b7f991590f
Adam Mróz <sadam36@gmail.com>
2013-05-04 15:41:13 +0200

    Another method for synonyms

--- src/main/java/pl/wwiizt/wordnet/WordnetJDBC.java
+++ src/main/java/pl/wwiizt/wordnet/WordnetJDBC.java
@@ -1,11 +1,12 @@
 package pl.wwiizt.wordnet;
 
 import java.io.IOException;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -15,7 +16,6 @@ public enum WordnetJDBC {
 	INSTANCE("connection.properties");
 
 	private Connection connection;
-	private Statement statement;
 
 	WordnetJDBC(String propertiesFileName) {
 		try {
@@ -42,7 +42,6 @@ public enum WordnetJDBC {
 			}
 
 			connection = DriverManager.getConnection(connectionURL.toString().trim());
-			statement = connection.createStatement();
 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -51,29 +50,75 @@ public enum WordnetJDBC {
 		}
 	}
 
-	public List<String> getSynonyms(String lemma) {
+	//to wyrzuca lematy, ktore sa polaczone z tym jakas relacja, tylko nie antonimem
+	public List<String> getAllLexInRelationsExceptAntonims(String lemma) {
 		List<String> result = new ArrayList<>();
+		String query = "select distinct lemma from lexicalunit where id  in (select parent_id from lexicalrelation where rel_id not in (select id from relationtype where name like 'antonimia%') and child_id in (select id from lexicalunit where lemma = ? ));";
 
-		ResultSet resultSet;
-		try {
-			StringBuilder sb = new StringBuilder("select lemma from lexicalunit where id  in (");
-			sb.append("select parent_id from lexicalrelation where rel_id not in (");
-			sb.append("select id from relationtype where name like 'antonimia%') and child_id in (");
-			sb.append("select id from lexicalunit where lemma = '");
-			sb.append(lemma);
-			sb.append("'));");
+		try (CallableStatement call = connection.prepareCall(query)) {
+			call.setString(1, lemma);
 
-			resultSet = statement.executeQuery(sb.toString());
+			ResultSet resultSet = call.executeQuery();
 
-			while (resultSet.next()) {
+			while (resultSet.next())
 				result.add(resultSet.getString("lemma"));
-			}
 
-			resultSet.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
 		return result;
 	}
+
+	//to wyrzuca lematy, ktore sa w tym samym synsecie
+	public List<String> getLexFromTheSameSynset(String lemma) {
+		List<Long> synsetsWithThisLemma = getSynsetsWithThisLemma(lemma);
+		List<String> result = new ArrayList<>();
+
+		if (synsetsWithThisLemma.isEmpty())
+			return result;
+					
+		//jdbc nie ma chyba jakiegos IN normalnego
+		StringBuilder query = new StringBuilder("select distinct lemma from lexicalunit where id in (select lex_id from unitandsynset where syn_id in (");
+
+		for (int i = 0; i < synsetsWithThisLemma.size() - 1; i++) {
+			query.append(synsetsWithThisLemma.get(i));
+			query.append(", ");
+		}
+
+		query.append(synsetsWithThisLemma.get(synsetsWithThisLemma.size() - 1));
+		query.append("))");
+
+		try (PreparedStatement lexes = connection.prepareCall(query.toString())) {
+			ResultSet resultSet = lexes.executeQuery();
+
+			while (resultSet.next())
+				result.add(resultSet.getString("lemma"));
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return result;
+	}
+
+	//synsety, ktore maja w sobie lemma
+	private List<Long> getSynsetsWithThisLemma(String lemma) {
+		List<Long> synsetsWithLemma = new ArrayList<>();
+
+		try (PreparedStatement synId = connection.prepareCall("select syn_id from unitandsynset where lex_id in (select id from lexicalunit where lemma = ?)")) {
+			synId.setString(1, lemma);
+			ResultSet rs = synId.executeQuery();
+
+			while (rs.next())
+				synsetsWithLemma.add(rs.getLong("syn_id"));
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return synsetsWithLemma;
+	}
+
 }
